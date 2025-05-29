352772df80ece833d1da5b5ddf0ae8cc24ca6ba3
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-05-29 10:40:55 +0300

    Feature: Adding validation to view model

--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
@@ -1,10 +1,17 @@
-﻿namespace LeaveManagementSystem.Web.Models.LeaveTypes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Models.LeaveTypes
 {
     // I don't need id in creating a new leave type.
     // It will be defined by the database.
     public class LeaveTypeCreateVM
     {
+        [Required]
+        [Length(4, 150, ErrorMessage="You have violated langth requirements")]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1,90)]
         public int NumberOfDays { get; set; }
     }
 }
