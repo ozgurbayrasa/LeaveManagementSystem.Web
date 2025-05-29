2a1c5684dc67421e8cfc78e456dab80823578c29
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-05-29 15:43:04 +0300

    Cleanup: Removing unnecesseary using statements.

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using LeaveManagementSystem.Web.Data;
 using LeaveManagementSystem.Web.Models.LeaveTypes;
-using AutoMapper;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using LeaveManagementSystem.Web.Services;
 
 namespace LeaveManagementSystem.Web.Controllers
